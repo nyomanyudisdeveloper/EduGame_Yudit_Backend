db4ec891ce125d4d525be099e1a70c1f64f87618
nyomanyudiswaramns <nyoman.yudiswara@mns.sch.id>
2026-04-04 22:46:48 +0700

    Fix game detail and modules

--- api/module/game/game.repository.tsx
+++ api/module/game/game.repository.tsx
@@ -11,16 +11,20 @@ export const getListGames = async () => {
 
 export const getGameById = async (id: string) => {
     const game = await sql`
-        SELECT id,name, description, thumbnail_link
-        FROM game_master
-        WHERE id = ${id}
+        SELECT a.id,a.name, a.description, a.thumbnail_link, b.name AS subject_name, c.name AS category_name
+        FROM game_master a
+        LEFT JOIN subject_master b
+        ON a.subject_id = b.id
+        LEFT JOIN category_master c
+        ON a.category_id = c.id
+        WHERE a.id = ${id}
     `;
     return game[0];
 }
 
 export const getListModuleGameById = async (id: string) => {
     const game = await sql`
-        SELECT b.id, b.name, b.description, b.level
+        SELECT b.id, b.name, b.description, b.level, b.thumbnail_link
         FROM game_master a 
         JOIN game_module b
         ON b.game_id = a.id
